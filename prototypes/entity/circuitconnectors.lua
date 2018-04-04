8ab9c4cce1f357e59027bba7fb3bd7c93b67385f
mspielberg <9729801+mspielberg@users.noreply.github.com>
2018-04-04 13:15:51 -0700

    Fix circuit wire appearance on proxies

--- prototypes/entity/circuitconnectors.lua
+++ prototypes/entity/circuitconnectors.lua
@@ -1,21 +1,14 @@
 local M = {}
 
-local function definition_with_offset(x, y)
-  return {
-    variation = 26,
-    main_offset = util.by_pixel(3 + x * 32, 5.5 + y * 32),
-    shadow_offset = util.by_pixel(7.5 + x * 32, 7.5 + y * 32),
-    show_shadow = true,
-  }
-end
+local chest_definition = { variation = 26, main_offset = util.by_pixel(3, 5.5), shadow_offset = util.by_pixel(7.5, 7.5), show_shadow = true }
 
 M["railloader-placement-proxy"] = circuit_connector_definitions.create(
   universal_connector_template,
   {
-    definition_with_offset( 0  , -1.5),
-    definition_with_offset( 1.5,  0  ),
-    definition_with_offset( 0  ,  1.5),
-    definition_with_offset(-1.5,  0  ),
+    chest_definition,
+    chest_definition,
+    chest_definition,
+    chest_definition,
   }
 )
 
